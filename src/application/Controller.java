7a6b0504ccd4f89c38b2909dc9b9d63022a2a5c8
Yannick Apostel <67263923+Yannick-Apostel@users.noreply.github.com>
2024-02-01 13:53:44 +0100

    get value from radiobutton

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,11 +2,16 @@ package application;
 
 import java.io.IOException;
 
+import application.WWM.Stufe;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -14,6 +19,7 @@ public class Controller {
 	private Scene scene;
 	private Parent root2;
 	
+	@FXML ToggleGroup   VBox; 
 	
 	
 	public void btndown_AnswA(ActionEvent e) {
@@ -59,6 +65,57 @@ public class Controller {
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+	}
+	
+	public void ssBestaetigen(ActionEvent e) throws IOException {
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("prevMenu.fxml"));
+		Parent root = loader.load();
+		
+		RadioButton selectedRadioButton = (RadioButton) VBox.getSelectedToggle();
+		String toogleGroupValue = selectedRadioButton.getText();
+		System.out.print(toogleGroupValue);
+	
+            Stufe stufe = new Stufe();
+            if (selectedRadioButton.getText().equals("50€"))        {
+                stufe = new Stufe(1);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(2);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(3);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(4);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(5);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(6);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(7);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(8);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(9);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(10);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(11);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(12);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(13);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(14);
+            } else if (selectedRadioButton.getText().equals("50€")) {
+                stufe = new Stufe(15);
+            }
+            /*if (aktuellesSpiel.getSicherheitsStufe1()==null) {
+                if (stufe.getLevel()!=0 && stufe.getGeld()!=0 && stufe.getGeldString()!=null) {
+                    aktuellesSpiel.setSicherheitsStufe1(stufe);
+                    
+				}
+          	}*/
+            Main.ssSetzen(stufe);
 	}
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
+
 	
 import application.WWM.altGUI;
+import application.WWM.Stufe;
+import application.WWM.WWM;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -9,7 +12,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
@@ -17,6 +19,7 @@ public class Main extends Application {
 
 			Parent root1 = FXMLLoader.load(getClass().getResource("prevMenu.fxml"));
 			Scene scene = new Scene(root1);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -25,15 +28,24 @@ public class Main extends Application {
 		}
 	}
 
+	/*public static void main(String[] args) {
+		launch(args);
+	}*/
+
+	
+	private static WWM wwm;
+
 	public static void main(String[] args) {
+		wwm = new WWM();
+		wwm.erstelleSpiel();
 		launch(args);
 	}
-
 	
-	
-
-	/*public static void main(String[] args) {
-		//altGUI test = new altGUI();
-		application.WWM.Read reader = new application.WWM.Read();
-	}*/
+	public static void ssSetzen(Stufe stufe) {
+		if (wwm.getAktuellesSpiel().getSicherheitsStufe1()==null) {
+            if (stufe.getLevel()!=0 && stufe.getGeld()!=0 && stufe.getGeldString()!=null) {
+                wwm.getAktuellesSpiel().setSicherheitsStufe1(stufe);
+			}
+      	}
+	}
 }
