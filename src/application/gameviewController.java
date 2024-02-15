04053abce451944e5fd20770bef53fd3a22351c5
Yannick Apostel <67263923+Yannick-Apostel@users.noreply.github.com>
2024-02-15 12:53:18 +0100

    neue fragen werden angezeigt

--- src/application/gameviewController.java
+++ src/application/gameviewController.java
@@ -14,18 +14,24 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class gameviewController {
-	
+
 	private Stage stage;
 	private Scene scene;
 	private Parent root2;
-	
-	@FXML Button btnAnswA; 
-	@FXML Button btnAnswB; 
-	@FXML Button btnAnswC; 
-	@FXML Button btnAnswD; 
-	@FXML Label label1;
-	
+
+	@FXML
+	Button btnAnswA;
+	@FXML
+	Button btnAnswB;
+	@FXML
+	Button btnAnswC;
+	@FXML
+	Button btnAnswD;
+	@FXML
+	Label label1;
+
 	Frage aktuelleFrage;
+
 	public void zeigeFrage(Frage frage) {
 		aktuelleFrage = frage;
 		label1.setText(frage.getText());
@@ -34,42 +40,67 @@ public class gameviewController {
 		btnAnswC.setText(frage.getAntwortenListe().get(2).getText());
 		btnAnswD.setText(frage.getAntwortenListe().get(3).getText());
 	}
-	
+
 	public void btndown_AnswA(ActionEvent e) throws IOException {
-		if(Main.antwortAuswertung(aktuelleFrage, 0) == false) {
+		if (Main.antwortAuswertung(aktuelleFrage, 0) == false) {
 			switchToTryAgain(e);
+		} else {
+			nächsteFrage();
 		}
 		;
 	}
-	public void btndown_AnswB(ActionEvent e) {
-		Main.antwortAuswertung(aktuelleFrage, 1);
-	}
-	public void btndown_AnswC(ActionEvent e) {
-		Main.antwortAuswertung(aktuelleFrage, 2);
-	}
-	public void btndown_AnswD(ActionEvent e) {
-		Main.antwortAuswertung(aktuelleFrage, 3);
-	}
-	
-public void switchToJoker(ActionEvent event) throws IOException {
-		
-	    root2 = FXMLLoader.load(getClass().getResource("Joker.fxml"));
-	    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root2);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
-        System.out.println("Succed!");
-	 } 
 
-public void switchToTryAgain(ActionEvent event) throws IOException {
-	
-    root2 = FXMLLoader.load(getClass().getResource("tryAgain.fxml"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    scene = new Scene(root2);
-    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-    stage.setScene(scene);
-    stage.show();
-    
- } 
+	public void btndown_AnswB(ActionEvent e) throws IOException {
+		if (Main.antwortAuswertung(aktuelleFrage, 1) == false) {
+			switchToTryAgain(e);
+		} else {
+			nächsteFrage();
+		}
+		;
+	}
+
+	public void btndown_AnswC(ActionEvent e) throws IOException {
+		if (Main.antwortAuswertung(aktuelleFrage, 2) == false) {
+			switchToTryAgain(e);
+		} else {
+			nächsteFrage();
+		}
+		;
+	}
+
+	public void btndown_AnswD(ActionEvent e) throws IOException {
+		if (Main.antwortAuswertung(aktuelleFrage, 3) == false) {
+			switchToTryAgain(e);
+		} else {
+			nächsteFrage();
+		}
+		;
+	}
+
+	public void nächsteFrage() {
+
+		zeigeFrage(Main.nF());
+	}
+
+	public void switchToJoker(ActionEvent event) throws IOException {
+
+		root2 = FXMLLoader.load(getClass().getResource("Joker.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root2);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+		System.out.println("Succed!");
+	}
+
+	public void switchToTryAgain(ActionEvent event) throws IOException {
+
+		root2 = FXMLLoader.load(getClass().getResource("tryAgain.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root2);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+
+	}
 }
